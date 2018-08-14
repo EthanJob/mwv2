2e02b2517dde48bba4cafa3d3169796b51f5e116
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-08-14 19:05:37 -0400

    main/home page works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 const app = angular.module('MomsWebsite', ['ngRoute']);
 
+app.controller('HomeController', ['$route', function () {
+
+}]);
+
 app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
+
+$locationProvider.html5Mode({ enabled: true });
+
+$routeProvider.when('/', {
+  templateUrl: 'home.html',
+  controller: 'HomeController',
+  controllerAs: 'ctrl'
+});
 
 }]);
