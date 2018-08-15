7011c90a47c2dd65ee83504a8a99950ae26532e1
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-08-15 13:35:54 -0400

    added reload function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const app = angular.module('MomsWebsite', ['ngRoute']);
 
 app.controller('HomeController', ['$route', function () {
 
+  this.reload = () => {
+    location.reload();
+  };
+
 }]);
 
 app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
@@ -11,6 +15,12 @@ $locationProvider.html5Mode({ enabled: true });
 $routeProvider.when('/', {
   templateUrl: 'pages/home.html',
   controller: 'HomeController',
+  controllerAs: 'ctrl'
+});
+
+$routeProvider.when('/dessert', {
+  templateUrl: 'pages/dessert.html',
+  controller: 'DessertController',
   controllerAs: 'ctrl'
 });
 
