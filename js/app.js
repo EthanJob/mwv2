bda2ac28966eb7b6228d16a47c48057515c37c7c
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-08-15 17:15:50 -0400

    cleaned up app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,6 @@ const app = angular.module('MomsWebsite', ['ngRoute']);
 
 app.controller('HomeController', ['$route', function () {
 
-  // this.reload = () => {
-  //   location.reload();
-  // };
-
 }]);
 
 app.controller('DessertController', ['$route', function () {
